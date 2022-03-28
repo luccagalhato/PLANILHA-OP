8bf14aff1cfa3c8f0a0d9b85ef10958202e7f006
Lucca Galhato <67028657+luccagalhato@users.noreply.github.com>
2022-03-28 18:48:42 -0300

    ...

--- api/apiHandlerAuth.go
+++ api/apiHandlerAuth.go
@@ -47,26 +47,26 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_, entries, _, err := auth.AuthenticateExtended(config, username, client.Userpassword, []string{"memberOf"}, nil)
-	// block := true
+	block := true
 	fmt.Println(entries)
 	if entries != nil {
 		for _, value := range entries.GetAttributeValues("memberOf") {
 			if strings.Contains(value, c.Yml.AUTH.Grupo) {
-				// block = false
+				block = false
 				break
 			}
 		}
 	}
 
-	// if block {
-	// 	w.WriteHeader(http.StatusUnauthorized)
-	// 	json.NewEncoder(w).Encode(models.Response{
-	// 		Status: "Unauthorized",
-	// 		Error:  "",
-	// 		Data:   "Senha ou username inválidos",
-	// 	})
-	// 	return
-	// }
+	if block {
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(models.Response{
+			Status: "Unauthorized",
+			Error:  "",
+			Data:   "Senha ou username inválidos",
+		})
+		return
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
